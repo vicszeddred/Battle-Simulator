0fa2d96b845fef5af49f72e36a97a438dfd5bfe9
Victor Ruiz Amaro <101755374+vicszeddred@users.noreply.github.com>
2022-09-27 22:16:24 -0500

    added more battle options functionality
    Still needing fixes

--- battle-simulator.py
+++ battle-simulator.py
@@ -7,6 +7,13 @@ def create_pokemon_team():
         pokemon_moves.trainer_red.capture_pokemon(pokemon_moves.all_pokemon[random.randint(0, len(pokemon_moves.all_pokemon) - 1)])
         count += 1
 
+def change_pokemon(pokemon_name):
+    selected_pokemon = ""
+    for pokemon in pokemon_moves.trainer_red.pokemon_team:
+        if pokemon_name == pokemon.species:
+            selected_pokemon = pokemon.species
+    print("Go {pokemon}".format(selected_pokemon))
+
 def wild_battle(difficulty):
     if difficulty.lower() == "easy":
         print("A wild {pokemon} has appeared.".format(pokemon = pokemon_moves.easy[random.randint(0, len(pokemon_moves.easy) - 1)]))
@@ -16,6 +23,13 @@ def wild_battle(difficulty):
         print("A wild {pokemon} has appeared.".format(pokemon = pokemon_moves.hard[random.randint(0, len(pokemon_moves.hard) - 1)]))
     print("go {mypokemon}!".format(mypokemon = pokemon_moves.trainer_red.pokemon_team[0]))
     option = input("What would you do? (battle  pokemon     run) ")
+    if option.lower() == "run":
+        print("you have run away")
+    if option.lower() == "pokemon":
+        for i in range(0, len(pokemon_moves.trainer_red.pokemon_team) - 1):
+            print("{i}.- {pokemon}".format(i = i + 1, pokemon = pokemon_moves.trainer_red.pokemon_team[i]))
+        poke_team = input("Choose a pokemon: ")
+        change_pokemon(poke_team.title())
     if option.lower() == "run":
         print("you have run away")
 
